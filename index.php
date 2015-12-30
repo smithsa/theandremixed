b0f2f221a21cd3c636830b5095361e778160030b
Sade <smithsade13@gmail.com>
2015-12-30 00:12:38 -0600

    adding files

---
+++ index.php
@@ -0,0 +1,197 @@
+<?php 
+header('Content-type: text/html');
+header('Access-Control-Allow-Origin: *');  
+?> 
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>The And Remixed</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,500italic,700,900' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
+        <link rel="stylesheet" type="text/css" href="css/animate.css" />
+       
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="bower_components/jquery/jquery.min.js"><\/script>')</script>
+        <script src="https://cdn.rawgit.com/nnattawat/flip/v1.0.18/dist/jquery.flip.min.js"></script>
+        <script src="js/jquery.fullPage.min.js"></script>
+        <script src="js/jquery.bgswitcher.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div id="fullpage">
+            <div class="section" id="section0">
+                <h1 id="intro-header">{&#10086;}</h1>
+                <h2 id="intro-subheader" >Raw. Honest. Conversation</h2><br/><br/>
+                <a href="#players" data-menuanchor="players" class="action-link">Play Game</a>
+            </div>
+            <div class="section" id="section1">
+                 <h2 class="white">Tell us who is playing?</h2>
+                 <select class="form-control" name="relationship" id="relationship"></select><br/><br/>
+                 <a id="relationship-next-btn" href="#directions" data-menuanchor="directions" class="action-link">Next</a>
+            </div>
+            <div class="section" id="section2">
+                   <div class="slide"> <h2 class="white game-directions">Game Directions</h2></div>
+                   <div class="slide"> <p class="white game-direction">Sit facing one another with a foot of distance seperating you.</p></div>
+                   <div class="slide"><p class="white game-direction">Person 1 begins by asking.<br/>Person 2 answers.</p></div>
+                   <div class="slide"> <p class="white game-direction">Continue back and forth <br/>through all <span id="number-of-questions"></span> questions.</p></div>
+                   <div class="slide"> <p class="white game-direction">Every question must be asked but you do not have to answer them. To earn the right to remain silent you must look the other person in the eye for 10 seconds before saying "pass."</p></div>
+                   <div class="slide"> <p class="white game-direction"  id="countdown-slide">
+                        <span id="countdown-instructions">Take 30 seconds to relax and look at one another. Begin after the bell rings.</span><br/><br/>
+                        <a href="javascript:void(0)" id="start-timer" class="action-link">Start Timer</a>
+                    </p></div>
+                
+            </div>
+            <div class="section" id="section3">
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>   
+                </div> <!-- 1 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 2 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 3 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 4 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 5 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 6 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 7 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 8 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 9 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 10 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 11 -->
+                <div class="slide question">
+                    <div class="card">
+                        <div class="front">
+                            <h1 class="white card-logo">{&#10086;}</h1>
+                        </div>
+                        <div class="back">
+                            <p class="relationship-question"></p>
+                        </div>
+                    </div>  
+                </div> <!-- 12 -->
+                <div class="slide">
+                    <h2 class="white game-over">Game Over <br/>
+                        <a href="#lastpage" data-menuanchor="lastpage" class="action-link">Credits</a> 
+                        <a href="javascript:void()" id="play-again" class="action-link">Play Again</a>
+                    </h2>    
+                </div>   
+            </div>
+            <div class="section" id="section4">
+                <h2 class="white">Credits</h2>
+                <p class="white">This game was inspired by the creators<br> of the interactive documentary, "The { } And".<br/>Visit the 
+                    <a href="http://theand.us/" class="credit-link">theand.us</a> for more information.</p>
+            </div>    
+        </div>
+
+    
+
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,165 @@
+       $(function() {
+            var number_of_questions_to_ask = 12;
+            //when changing the number of slides add the equal amount of divs to the page
+            var hostsite = "http://theand.site88.net/";
+            
+            $("#number-of-questions").text(number_of_questions_to_ask);
+
+            $.ajax({
+                  method: "GET",
+                  url: hostsite+"api/api.php/relationship",
+                  dataType: "json",
+                  crossDomain : true,
+                  success: function(response){
+                    var relationships = response['relationship']['records']; 
+                    for(var i=0; i < relationships.length; i++){
+                        $('#relationship').prepend(new Option(relationships[i][1], relationships[i][0]));
+                    }
+                  }
+            });
+
+             $.ajax({
+                      method: "GET",
+                      url: hostsite+"api/api.php/relationship_question,question,?filter=relationship_id,eq,"+1,
+                      dataType: "json",
+                      crossDomain : true,
+                      success: function(response){
+
+                            var num_questions = response["question"]["records"].length
+                            var unfilted_questions = response["question"]["records"];
+                            var list_of_questions = [];
+                            var shuffled_array = shuffle(unfilted_questions);
+                            console.log(shuffled_array);
+                            for(var i = 0; i < number_of_questions_to_ask; i++){
+                                list_of_questions.push( [shuffled_array[i][2], shuffled_array[i][1] ]);
+                            }
+
+                            list_of_questions.sort(sortFunction);
+                           
+                            var count = 0; 
+                            $("#section3 .question .relationship-question").each(function( ){
+                                $(this).append(unescape(list_of_questions[count][1]));
+                                count++;
+                            });
+
+                      }
+                });
+
+            $('#fullpage').fullpage({
+                verticalCentered: true,
+                sectionsColor: ['#000000', '#8e44ad', '#2980b9', '#E29C45', '#c0392b'],
+                anchors: ['home', 'players', 'directions', 'questions', 'credits']
+            });
+
+            //loading relationship dropdown
+            $("#relationship-next-btn").on("click",  function(){
+                $.ajax({
+                      method: "GET",
+                      url: hostsite+"api/api.php/relationship_question,question,?filter=relationship_id,eq,"+$( "#relationship" ).val(),
+                      dataType: "json",
+                      crossDomain : true,
+                      success: function(response){
+                            var num_questions = response["question"]["records"].length
+                            var unfilted_questions = response["question"]["records"];
+                            var list_of_questions = [];
+                            var shuffled_array = shuffle(unfilted_questions);
+                            console.log(shuffled_array);
+                            for(var i = 0; i < number_of_questions_to_ask; i++){
+                                list_of_questions.push( [shuffled_array[i][2], shuffled_array[i][1] ]);
+                            }
+
+                            list_of_questions.sort(sortFunction);
+                            console.log(list_of_questions);
+                            for(var j =0; j < list_of_questions.length; j++){
+                                var slider_container = $('<div/>').attr({ class: 'slide fp-slide fp-table'});
+                                var fp_container = $('<div/>').attr({ class: 'fp-tableCell'});
+                                var card_container = $('<div/>').attr({ class: 'card'});
+                                var front_side_of_card = $('<div/>').attr({ class: 'front'}).html('<h1 class="white card-logo">{&#10086;}</h1>');
+                                var back_side_of_card = $('<div/>').attr({ class: 'back'}).html('<p>'+ unescape(list_of_questions[1]) +'</p>');
+                                card_container.append(front_side_of_card, back_side_of_card);
+                                fp_container.append(card_container);
+                                slider_container.append(fp_container);
+                                $(".fp-slides .fp-slidesContainer").append(slider_container);
+
+                            }
+
+                      }
+                });
+
+            });
+
+            $("#section0").bgswitcher({
+              images: [ "images/background1.png", 
+                        "images/background2.png", 
+                        "images/background3.png", 
+                        "images/background4.png", 
+                        "images/background5.png"
+                      ]});
+
+            $.fn.fullpage.setMouseWheelScrolling(false);
+            $.fn.fullpage.setAllowScrolling(false);
+
+             $("#start-timer").on("click", function(){
+                $(this).css("visibility", "hidden");
+                game_countdown(31);
+             });
+         
+            $(".card").flip({axis: 'x'});
+           
+            $("#play-again").on("click", function(){
+                window.location.replace(hostsite+"#players");
+            });
+
+            function shuffle(array) {
+                  var currentIndex = array.length, temporaryValue, randomIndex;
+
+                  // While there remain elements to shuffle...
+                  while (0 !== currentIndex) {
+
+                    // Pick a remaining element...
+                    randomIndex = Math.floor(Math.random() * currentIndex);
+                    currentIndex -= 1;
+
+                    // And swap it with the current element.
+                    temporaryValue = array[currentIndex];
+                    array[currentIndex] = array[randomIndex];
+                    array[randomIndex] = temporaryValue;
+                  }
+
+                  return array;
+            }
+
+            function game_countdown(count){
+                var count = count;
+                var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+                function timer(){
+                    count=count-1;
+                    if (count <= 0){
+                        clearInterval(counter);
+                        $("#countdown-instructions").fadeOut(function() {
+                                $(this).text("You may now begin the game!");
+                                $("#start-timer").fadeOut(function() {
+                                    $("#start-timer").css("visibility", "visible");
+                                    $("#start-timer").text("Start Game");
+                                    $("#start-timer").attr("href", "#questions");
+                                    $("#start-timer").attr("id", "#start_game");
+                                }).fadeIn(); 
+                        }).fadeIn();  
+                        var audio = new Audio('sound/ding.mp3');
+                        audio.play();
+                        for(i=0;i<16;i++) {$("#section2").fadeTo('slow', 0.5).fadeTo('slow', 1.0);}   
+                        return;
+                    }
+                }
+            }
+
+            function sortFunction(a, b) {
+                if (a[0] === b[0]) {
+                    return 0;
+                }
+                else {
+                    return (a[0] < b[0]) ? -1 : 1;
+                }
+            }
+
+        });
