051f3a07bd253f57e49aed291dfb43b41682ebaa
Sade <smithsade13@gmail.com>
2015-12-30 11:50:32 -0600

    adding config

---
+++ api/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'hostname' => 'us-cdbr-iron-east-03.cleardb.net',
+    'username' => 'bff3212d249702',
+    'password' => '16f82a75',
+    'database' => 'heroku_69282bb8e54b040',
+);
